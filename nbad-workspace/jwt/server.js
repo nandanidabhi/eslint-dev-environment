684f3f735185bc92b18b31ba4d801663fdf76475
nandanidabhi <nandanidabhi2@gmail.com>
2020-10-23 19:45:28 -0400

    NodeJS JWT Authentication Test

--- nbad-workspace/jwt/server.js
+++ nbad-workspace/jwt/server.js
@@ -5,7 +5,52 @@ const jwt =  require('jsonwebtoken');
 const exjwt = require('express-jwt');
 const bodyParser = require('body-parser');
 const path = require('path');
+var decoded={};
+var tokenCheck;
 
+var verifyToken = function(req,res,next){
+    var tokenValue = req.header('authorization').split(" ")[1];
+    // console.log(tokenValue);
+    if(!tokenValue){
+        res.json({
+            success: false,
+            myContent: "Invalid Token. Please login again!"
+        });
+    }
+
+    var authData = req.header('authorization').split(" ")[1];
+    if(authData){
+        tokenCheck = authData;
+        try {
+            decoded = jwt.verify(tokenCheck, secretKey);
+            if(!decoded){
+                res.json({
+                    success: false,
+                    myContent: "Invalid Token. Please login again!"
+                });
+            }
+            if(!decoded.username){
+                res.json({
+                    success: false,
+                    myContent: "Unauthorized User. Please check your credentials!"
+                });
+            }
+            next();
+          } catch(err) {
+            // return res.status(400).send('<script>alert("'+err.toString()+'");</script>');
+            res.json({
+                success: false,
+                myContent: err.toString()+" Please login again"
+            });
+          }
+        }else{
+            res.json({
+                success: false,
+                myContent: "Please login again"
+            });
+        }
+
+};
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
@@ -32,17 +77,19 @@ let users = [
     },
     {
         id: 2,
-        username: "Dabhi",
-        password: "456"
+        username: 'Dabhi',
+        password: '456' 
     }
 ];
+   
 
 app.post('/api/login', (req, res) => {
     const {username, password } = req.body;
-    
+    let inside = false;
     for(let user of users){
+        console.log("in Users"+users);
         if(username == user.username && password == user.password){
-            let token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: "3m" });
+            let token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '3m' });
                 res.json({
                     success: true,
                     err: null,
@@ -50,17 +97,17 @@ app.post('/api/login', (req, res) => {
                 });
                 break;
             }
-            else{
-                res.status(401).json({
-                    success: false,
-                    token: null,
-                    err: 'Username or password is incorrect'
-                });
         }
-    }
-});
+        if(!inside){
+            res.status(401).json({
+                success: false,
+                token: null,
+                err: 'username or password is incorrect'
+            });
+        }
+    });
 
-app.get('/api/dashboard', jwtMW, (req, res) => {
+app.get('/api/dashboard', verifyToken, (req, res) => {
     console.log(res);
     res.json({
         success: true,
@@ -68,7 +115,7 @@ app.get('/api/dashboard', jwtMW, (req, res) => {
     });
 });
 
-app.get('/api/prices', jwtMW, (req, res) => {
+app.get('/api/prices', (req, res) => {
     console.log(res);
     res.json({
         success: true,
@@ -76,13 +123,14 @@ app.get('/api/prices', jwtMW, (req, res) => {
     });
 });
 
-app.get('/api/settings', jwtMW, (req, res) => {
+app.get('/api/settings',verifyToken , (req, res) => {
     console.log(res);
     res.json({
         success: true,
         myContent: 'Welcome to Settings'
     });
 });
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -102,6 +150,7 @@ app.use(function (err,req, res,next) {
         next(err);
     }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Serving on port ${PORT}`);
