4ac0bcfc0db6fe7ac3b5e2efad7a769ccc538845
nandanidabhi <nandanidabhi2@gmail.com>
2020-11-12 13:48:45 -0500

    first commit of the old files to set up the environment

--- nbad-workspace/week02/my-server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req,res){
-    res.write('Hello World from Nandani Dabhi!!');
-    res.end();
-}).listen(3000, function(){
-console.log("Server running at port 3000");
-});
